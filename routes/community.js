dc8b92af33fb60384baf9772f2df19d42068f178
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-05-12 07:55:09 +0200

    stuff i guess

--- routes/community.js
+++ routes/community.js
@@ -701,7 +701,7 @@ module.exports = function (app, dir) {
                     discord.sendEmbed(channel, {
                         title: req.body.title,
                         color: 0x00ffff,
-                        description: turndown.turndown(req.body.content),
+                        description: turndown(req.body.content),
                         fields: [{
                             name: "Link:",
                             value: `http://sonwoojin.com/community/board/${req.params.board}/${result[0].id}`,
